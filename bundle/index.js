a8e6869aa188e6b9acfff2a2e363800b5582d749
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-09-07 20:19:12 +0700

    Tipe data custom

--- bundle/index.js
+++ bundle/index.js
@@ -3,6 +3,24 @@ let namaSaya = "Deft Valian Exanova";
 let usernaame = 123;
 ;
 let isDead = false;
+let pacarSaya;
+pacarSaya = "Nabila Fitriyani";
+pacarSaya = 1;
+// array biasa
+let istriSaya;
+istriSaya = ["Nabila", "Fitriyani"];
+// tupple type
+let legendFavorit;
+legendFavorit = ["Hattori", 30, true];
+let temanKita;
+temanKita = {
+    nama: "Rangga",
+    isKampret: true,
+    hutang: 45000,
+};
+console.log({ temanKita });
 console.log({ namaSaya });
 console.log({ usernaame });
 console.log({ isDead });
+console.log({ istriSaya });
+console.log({ legendFavorit });
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,32 @@ let pacarSaya: string | number;
 pacarSaya = "Nabila Fitriyani";
 pacarSaya = 1;
 
+// array biasa
+let istriSaya: string[];
+istriSaya = ["Nabila", "Fitriyani"];
+
+// tupple type
+let legendFavorit: [string, number, boolean];
+legendFavorit = ["Hattori", 30, true];
+
+
+//! TIPEDATA CUSTOM
+type TemanType = {
+    nama: string;
+    isKampret: boolean;
+    hutang?: number;
+};
+let temanKita: TemanType;
+temanKita = {
+    nama: "Rangga",
+    isKampret: true,
+    hutang: 45_000,  
+};
+
+console.log({ temanKita })
+
 console.log({ namaSaya });
 console.log({ usernaame });
 console.log({ isDead });
+console.log({ istriSaya });
+console.log({ legendFavorit });
